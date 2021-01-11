44e9fedcc40837a28df10e58e70773df528820a5
Ashok Parasa <ashingtondc@gmail.com>
2021-01-11 13:55:14 -0500

    Added state and delete functionality

--- src/App.js
+++ src/App.js
@@ -2,33 +2,47 @@ import React, { Component } from 'react'
 import Table from "./Table"
 
 class App extends Component {
-    render() {
-      const characters = [
-        {
-          name: 'Charlie',
-          job: 'Janitor',
-        },
-        {
-          name: 'Mac',
-          job: 'Bouncer',
-        },
-        {
-          name: 'Dee',
-          job: 'Aspring actress',
-        },
-        {
-          name: 'Dennis',
-          job: 'Bartender',
-        },
-      ]
-  
-      return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      )
-    }
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ],
   }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  render() {
+    const { characters } = this.state;
+    
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+      </div>
+    );
+  }
+}
 
 // makes the component available to be imported into other components or files
 export default App
